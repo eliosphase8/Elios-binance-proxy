cf328fe71c4ab447c22605a69a6413a395ba1dd8
eliosphase8 <mducanda.ar@gmail.com>
2025-05-14 02:43:52 +0200

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+ from flask import Flask, jsonify
 from binance.client import Client
 import os
 
@@ -20,4 +20,5 @@ def get_balance():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
